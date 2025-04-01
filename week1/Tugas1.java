553f2c7d70ad69c3996e0b866f622e82dda39d37
Muiz Firmansyah <muiz14510@gmail.com>
2025-04-01 19:41:03 +0700

    inital commit

---
+++ week1/Tugas1.java
@@ -0,0 +1,14 @@
+package week1;
+
+class Tugas1{
+    public static void main(String[] args){
+        System.out.println("NIM: A11.2023.15416");
+        System.out.println("Nama: Muiz Firmansyah");
+        System.out.println("Alamat: Jl. Lempongsari 1 No.407, Lempongsari, Kec. Gajahmungkur");
+        System.out.println("Kota: Semarang");
+        System.out.println("Kode Pos: 50231");
+        System.out.println("Telp. : -");
+        System.out.println("HP: 089669752343");
+        System.out.println("EMail: muiz14510@gmail.com");
+    }
+}
---
+++ week1/Tugas2.java
@@ -0,0 +1,46 @@
+package week1;
+
+class Tugas2{
+    static float Pi = 3.14f;
+    public static void main(String[] args){
+        System.out.println("Luas Persegi: " + lpersegi(3));
+        System.out.println("Keliling Persegi: " + kpersegi(3));
+        System.out.println("Luas Segitigas: " + lsegitiga(3, 5));
+        System.out.println("Keliling Segitiga: " + ksegitiga(3, 3, 3));
+        System.out.println("Luas Lingkaran: " + llingkaran(7));
+        System.out.println("Keliling Lingkaran: " + klingkaran(7));
+        System.out.println("Luas Kubus: " + lkubus(5));
+        System.out.println("Keliling Kubus: " + kkubus(5));
+    }
+
+    public static float lpersegi(float s){
+        return s * s;
+    }
+
+    public static float kpersegi(float s){
+        return 4 * s;
+    }
+
+    public static float lsegitiga(float a, float t){
+        return a * t / 2;
+    }
+
+    public static float ksegitiga(float a, float b, float c){
+        return a + b + c;
+    }
+    public static float llingkaran(float r){
+        return Pi * r * r;
+    }
+
+    public static float klingkaran(float r){
+        return 2 * Pi * r;
+    }
+
+    public static float lkubus(float s){
+        return 6 * s * s;
+    }
+
+    public static float kkubus(float s){
+        return 12 * s;
+    }
+}
